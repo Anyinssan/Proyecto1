136b3651b15fe0b49270eef523fc19c4cf4299ee
anyinssan <anjin95navasan@gmail.com>
2023-06-27 17:22:05 -0600

    Hice varios cambios

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+import datetime
 
 def saludo(request):
     
@@ -7,3 +8,34 @@ def saludo(request):
 def despedida(request):
     
     return HttpResponse("Hasta luego alumnos de Django") 
+
+def dameFecha(request):
+    
+    fecha_actual = datetime.datetime.now()
+    
+    documento ="""
+    <html>
+    <body>
+    <h2>
+    Fecha y hora actuales %s
+    </h2>
+    </body>
+    </html> """ % fecha_actual
+    
+    return HttpResponse(documento) 
+
+def calculaEdad(request,edad,agno):
+    
+    periodo = agno - 2023
+    edadFutura = edad+ periodo
+    documento = """
+    <html>
+    <body>
+    <h2>
+    En el año %s tendrás %s años 
+    </h2>
+    </body>
+    </html> 
+    """ %(agno,edadFutura)
+    
+    return HttpResponse(documento)
